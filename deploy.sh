0028127343297d15eecc3df880cb650a767c118e
Matt Sparks <ms@quadpoint.org>
2010-08-03 23:24:41 -0700

    Adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd `dirname $0`
+
+case $1 in
+  "prod")
+    site="quadpoint.org"
+    ;;
+  "beta")
+    site="beta.quadpoint.org"
+    ;;
+  *)
+    echo "usage: $0 beta|prod"
+    exit 1
+    ;;
+esac
+
+rsync -avz --progress --delete _site/* quadpoint.org:public/$site/
